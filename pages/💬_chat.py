108b4dc7d2a288e2d41b4ee9bf474f178ace21a6
manikandanp <manikandanp@softsquare.biz>
2023-10-19 18:35:45 +0530

    added docs

--- pages/💬_chat.py
+++ pages/💬_chat.py
@@ -11,11 +11,35 @@ import streamlit as st
 from streamlit_chat import message
 from utils import *
 from dotenv import load_dotenv
+import user_info as UserInputs
 
 load_dotenv()
 
+# st.markdown(
+#     """
+#         <style>
+#         button {
+#             height: 10px;
+#             padding-top: 0px !important;
+#             padding-bottom: 0px !important;
+#         }
+#         </style>
+#         """,
+#             unsafe_allow_html=True,
+# )
 
-st.title("Chat with PDF 💬")
+st.title("Chat with AI 💬")
+st.write("I have information about the content below. You can ask me questions about it.")
+
+product_type = st.sidebar.selectbox("Product", UserInputs.getProductList())
+
+user_input = {
+    "product_type": product_type
+}
+
+print('user_input ::::', user_input)
+
+st.session_state["user_input"] = user_input
 
 if 'responses' not in st.session_state:
     st.session_state['responses'] = []
@@ -35,6 +59,7 @@ if 'buffer_memory' not in st.session_state:
 system_msg_template = SystemMessagePromptTemplate.from_template(template="""Answer the question as truthfully as possible using the provided context, 
 and if the answer is not contained within the text below, say 'I don't know'""")
 
+#  if answer contains 'Based on the provided context,' pleace remove that sentence
 
 human_msg_template = HumanMessagePromptTemplate.from_template(template="{input}")
 
@@ -51,7 +76,7 @@ textcontainer = st.container()
 print('Initial Load ::::::::', st.session_state.initialPageLoad)
 if(st.session_state.initialPageLoad):
     print('if Initial Load ::::::::', st.session_state.initialPageLoad)
-    refined_query = 'summarize the document and genrate some meaningfull qustion based on the document'
+    refined_query = f"""summarize the document and genrate some meaningfull qustion based on the document in {product_type}"""
     context = find_match(refined_query)
     response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{refined_query}")
     st.session_state.responses.append(response) 
@@ -59,22 +84,21 @@ if(st.session_state.initialPageLoad):
 
 with textcontainer:
     st.session_state.initialPageLoad = False
-    query = st.text_input("Query: ", key="input")
+    query = st.chat_input("Say something ... ", key="input")
     if query:
         print('query :::', query)
+        conversation_string = get_conversation_string()
+        print('conversation_string ::::: ::: ',conversation_string)
+        # st.code(conversation_string)
         with st.spinner("typing..."):
-            conversation_string = get_conversation_string()
-            print('conversation_string ::::: ::: ',conversation_string)
-            # st.code(conversation_string)
             refined_query = query_refiner(conversation_string, query)
             print('refined_query :::::', refined_query)
             st.subheader("Refined Query:")
             st.write(refined_query)
             context = find_match(refined_query)
-            # print(context)  
             response = conversation.predict(input=f"Context:\n {context} \n\n Query:\n{query}")
-        st.session_state.requests.append(query)
-        st.session_state.responses.append(response) 
+            st.session_state.requests.append(query)
+            st.session_state.responses.append(response) 
 with response_container:
     st.session_state.initialPageLoad = False
     if st.session_state['responses']:
