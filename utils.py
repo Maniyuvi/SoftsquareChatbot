320dcbb441520ea9055bfbaa892878ed2da7f7a7
Manikandan P <manikandanp@softsquare.biz>
2024-01-17 12:48:09 +0530

    Updated UI for chnage Product

--- utils.py
+++ utils.py
@@ -39,7 +39,7 @@ def query_refiner(conversation, query):
             query = query + f"in {product_type}"
 
     response = openai.Completion.create(
-    model="text-davinci-003",
+    model="gpt-3.5-turbo-instruct",
     prompt=f"Given the following user query and conversation log, formulate a question that would be the most relevant to provide the user with an answer from a knowledge base.\n\nCONVERSATION LOG: \n{conversation}\n\nQuery: {query}\n\nRefined Query:",
     temperature=0.7,
     max_tokens=256,
